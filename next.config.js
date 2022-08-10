bb7cda96506c18ea6bf95706f93952cbfc428e8f
Fernando <ferobregon03@gmail.com>
2022-08-10 17:00:20 +0100

    changed to test on localhost

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/Tapadoo-site',
-  assetPrefix: '/Tapadoo-site',
+  basePath: isProd ? '/Tapadoo-site' : '',
+  assetPrefix: isProd ? '/Tapadoo-site' : '',
 };
 
 module.exports = nextConfig;
