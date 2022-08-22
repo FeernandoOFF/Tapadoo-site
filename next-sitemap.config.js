94e4bf6b7fe60e35ef4d02cb702e86472ae2b30f
Fernando <ferobregon03@gmail.com>
2022-08-22 16:21:22 +0100

    Changed path

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://feernandooff.github.io/',
+  siteUrl:
+    process.env.SITE_URL || 'https://feernandooff.github.io/Tapadoo-site/',
   generateRobotsTxt: true, // (optional)
+  // ...other options
 };
